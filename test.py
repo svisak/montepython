aa72ef9de911f4888f304782b738fb639e091fc7
Isak Svensson <isak.svensson@chalmers.se>
2019-04-23 09:50:06 +0200

    Small improvements to test script

--- test.py
+++ test.py
@@ -22,16 +22,23 @@ startpos = np.zeros(dim)
 stepsize = 2.5
 
 rwm = RandomWalkMetropolis(stepsize, dim, startpos, prior, likelihood)
-rwm.run(20000)
+rwm.run(10000)
 
-plt.hist(rwm.get_chain(), bins=500)
-#plt.savefig('tmp.pdf', bbox_inches='tight')
+plt.figure(figsize=(4.5, 3.0))
+x = np.linspace(-3, 3)
+y = np.sqrt(1/2/np.pi) * np.exp(-0.5 * x**2)
+plt.plot(x, y)
+
+plt.hist(rwm.get_chain(), bins=500, density=True, stacked=True)
+plt.savefig('r.pdf', bbox_inches='tight')
 
 hmc = HMC(gradient, 30, 0.001, dim, startpos, prior, likelihood)
-hmc.run(20000)
+hmc.run(10000)
 
-plt.hist(hmc.get_chain(), bins=500)
-plt.savefig('tmp.pdf', bbox_inches='tight')
+plt.figure(figsize=(4.5, 3.0))
+plt.hist(hmc.get_chain(), bins=500, density=True, stacked=True)
+plt.plot(x, y)
+plt.savefig('h.pdf', bbox_inches='tight')
 
 print(hmc.acceptance_rate())
 print(rwm.acceptance_rate())
