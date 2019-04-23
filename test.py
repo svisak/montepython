aaa88320b33d8296dc5e21e5144f5488832a4bdb
Isak Svensson <isak.svensson@chalmers.se>
2019-04-23 11:14:04 +0200

    Increased step size, much better result

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ dim = 1
 startpos = np.zeros(dim)
 stepsize = 2.5
 
+
 rwm = RandomWalkMetropolis(stepsize, dim, startpos, prior, likelihood)
 rwm.run(10000)
 
@@ -30,15 +31,20 @@ y = np.sqrt(1/2/np.pi) * np.exp(-0.5 * x**2)
 plt.plot(x, y)
 
 plt.hist(rwm.get_chain(), bins=500, density=True, stacked=True)
-plt.savefig('r.pdf', bbox_inches='tight')
+plt.savefig('rwm.pdf', bbox_inches='tight')
 
-hmc = HMC(gradient, 30, 0.001, dim, startpos, prior, likelihood)
+hmc = HMC(gradient, 30, 0.1, dim, startpos, prior, likelihood)
 hmc.run(10000)
 
 plt.figure(figsize=(4.5, 3.0))
 plt.hist(hmc.get_chain(), bins=500, density=True, stacked=True)
 plt.plot(x, y)
-plt.savefig('h.pdf', bbox_inches='tight')
+plt.savefig('hmc.pdf', bbox_inches='tight')
+
+plt.figure(figsize=(4.5, 3.0))
+plt.hist(rwm.get_chain(), bins=500)
+plt.hist(hmc.get_chain(), bins=500)
+plt.savefig('both.pdf', bbox_inches='tight')
 
 print(hmc.acceptance_rate())
 print(rwm.acceptance_rate())
