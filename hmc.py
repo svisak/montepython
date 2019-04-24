Fixed mistake in test gradient

hmc.py
@@ -37,7 +37,7 @@ class HMC(montepython.MontePython):
 
     def run(self, n_steps):
         self.chain.extend(n_steps)
-        momentum = np.zeros(self.chain.dimensionality())
+        momentum = np.empty(self.chain.dimensionality())
         for i in range(1, n_steps):
             proposed_position, proposed_momentum = self.propose()
 
@@ -63,6 +63,8 @@ class Leapfrog():
         self.gradient = gradient
         self.ell = ell
         self.epsilon = epsilon
+        self.all_positions = []
+        self.all_momenta = []
 
     def draw_ell(self):
         return self.ell
@@ -73,15 +75,23 @@ class Leapfrog():
         # return self.epsilon + 0.1*self.epsilon*np.random.rand()
 
     def solve(self, position, momentum):
+        self.all_positions = []
+        self.all_momenta = []
+        self.all_positions.append(position)
+        self.all_momenta.append(momentum)
         ell = self.draw_ell()
         epsilon = self.draw_epsilon()
 
         # SOLVE AND RETURN
         momentum = momentum - epsilon * self.gradient(position) / 2
+        #self.all_momenta.append(momentum)
         for i in range(1, ell):
             position = position + epsilon * momentum
+            self.all_positions.append(position)
             if (i != ell):
                 momentum = momentum - epsilon * self.gradient(position)
+                self.all_momenta.append(momentum)
         momentum = momentum - epsilon * self.gradient(position) / 2
+        #self.all_momenta.append(momentum)
         momentum = -momentum
         return (position, momentum)

test.py
@@ -13,7 +13,7 @@ def lnlikelihood(q):
     return -0.5 * q**2
 
 def gradient(q):
-    return -q*np.exp(-0.5 * q**2)
+    return -np.sqrt(1/2/np.pi) * q * np.exp(-0.5 * q**2)
 
 # Run parameters
 dim = 1