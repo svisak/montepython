90b805831966145d63852eb289a93d3b1ca44865
Isak Svensson <isak.svensson@chalmers.se>
2019-04-30 08:18:48 +0200

    Improved test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import numpy as np
 from rwm import RWM
 from hmc import HMC
 
-var = 4.2
-mu = 3
+var = 2.0
+mu = 4
 
 # Specify distributions
 def lnprior(q):
@@ -28,47 +28,28 @@ dim = 1
 startpos = 0.1 * np.ones(dim)
 
 # A normal distribution
-x = np.linspace(-4, 8)
+x = np.linspace(0, 8)
 y = np.sqrt(1/2/np.pi/var) * np.exp(-0.5 * (x-mu)**2/var)
 
 def rwm_test():
-    stepsize = 4.5
+    sigma = 7.0
     rwm = RWM(stepsize, dim, startpos, lnprior, lnlikelihood)
-    rwm.run(10000)
+    rwm.run(100000)
     print('Acceptance rate RWM: {}'.format(rwm.acceptance_rate()))
 
     plt.figure(figsize=(4.5, 3.0))
     plt.plot(x, y)
 
-    plt.hist(rwm.get_chain(), bins=500, density=True, stacked=True)
-    plt.savefig('fig/rwm.pdf', bbox_inches='tight')
-
-def hmc_test():
-    hmc = HMC(gradient, 200, 0.2, dim, startpos, lnprior, lnlikelihood)
-    var = 1.0
-    mi = 1/var/var
-    hmc.scale_mass_matrix(mi)
-    hmc.run(20000)
-    print('Acceptance rate HMC: {}'.format(hmc.acceptance_rate()))
-
-    plt.figure(figsize=(4.5, 3.0))
-    plt.hist(hmc.get_chain(), bins=250, density=True, stacked=True)
-    plt.plot(x, y)
+    plt.hist(rwm.get_chain(), bins=300, density=True, stacked=True)
     plt.xlabel(r'$q$')
     plt.ylabel(r'$\pi(q)$')
-    plt.title(r'HMC, $m_i = {}$, acceptance\_rate $= {}$'.format(mi, hmc.acceptance_rate()))
-    plt.savefig('fig/hmc.pdf', bbox_inches='tight')
+    plt.title(r'RWM, sigma $= {}$, acceptance\_rate $= {}$'.format(sigma, rwm.acceptance_rate()))
+    plt.savefig('fig/rwm_sigma{}.pdf'.format(sigma), bbox_inches='tight')
 
-def both_test():
-    rwm_test()
-    hmc_test()
-    plt.figure(figsize=(4.5, 3.0))
-    plt.hist(rwm.get_chain(), bins=500, density=True, stacked=True)
-    plt.hist(hmc.get_chain(), bins=500, density=True, stacked=True)
-    plt.savefig('fig/both.pdf', bbox_inches='tight')
-
-def hmc_test_tmp():
-    hmc = HMC(gradient, 200, 0.2, dim, startpos, lnprior, lnlikelihood)
+def hmc_test():
+    ell = 400
+    epsilon = 0.2
+    hmc = HMC(gradient, ell, epsilon, dim, startpos, lnprior, lnlikelihood)
     mi = 1.0
     hmc.scale_mass_matrix(mi)
     hmc.run(100000)
@@ -79,7 +60,8 @@ def hmc_test_tmp():
     plt.plot(x, y)
     plt.xlabel(r'$q$')
     plt.ylabel(r'$\pi(q)$')
-    plt.title(r'HMC, $m_i = {}$, acceptance\_rate $= {}$'.format(mi, hmc.acceptance_rate()))
-    plt.savefig('fig/hmc_mi{}.pdf'.format(mi), bbox_inches='tight')
+    plt.title(r'HMC, $m_i = {}$, acc\_rate $= {}, L = {}, \epsilon = {}$'.format(mi, hmc.acceptance_rate(), ell, epsilon))
+    plt.savefig('fig/hmc_mi{}_L{}_eps{}.pdf'.format(mi, ell, epsilon), bbox_inches='tight')
 
-hmc_test_tmp()
+rwm_test()
+hmc_test()
