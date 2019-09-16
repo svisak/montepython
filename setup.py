624e9e1698d6f3f6f71f2540962da613cc418d54
Isak Svensson <isak.svensson@chalmers.se>
2019-09-16 18:23:31 +0200

    setup.py: Improved handling of version by bypassing the need to write a temporary file. Also corrected indentation.

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-import os
+import subprocess
 
 with open('README') as f:
     readme = f.read()
 
-os.system('git describe > VERSION')
-version_file = open('VERSION')
-version = version_file.read().strip()
+#cmd = 'git describe --abbrev=0'
+#version = subprocess.check_output(cmd, shell=True).decode('utf-8')
+cmd = 'git describe --dirty'
+dirty_version = subprocess.check_output(cmd, shell=True).decode('utf-8')
 
 setup(
-   name='montepython',
-   version=version,
-   description='Markov chain Monte Carlo algorithms',
-   author='Isak Svensson',
-   author_email='isak.svensson@chalmers.se',
-   packages=['montepython'],  #same as name
-   install_requires=['numpy', 'scipy'], #external packages as dependencies
+    name='montepython',
+    version=dirty_version,
+    description='Markov chain Monte Carlo algorithms',
+    author='Isak Svensson',
+    author_email='isak.svensson@chalmers.se',
+    packages=['montepython'],  #same as name
+    install_requires=['numpy', 'scipy'], #external packages as dependencies
 )
