10742b1933e870740b96228126556bfc27dfeac8
Isak Svensson <isak.svensson@chalmers.se>
2019-09-28 19:43:21 +0200

    Added a docstring to the autocorrelation diagnostic utility, and fixed an error in the autocorrelation_time function.

--- montepython/diagnostics.py
+++ montepython/diagnostics.py
@@ -1,10 +1,16 @@
-# MCMC utilities.
+# MCMC diagnostic utilities.
 
 import warnings
 import numpy as np
 import scipy
 
 def autocorrelation(chain, max_lag):
+    """
+    Return an ndarray containing the autocorrelations for each
+    dimension of the chain separately.
+
+    The shape of the returned array is (max_lag+1, ndim).
+    """
     ndim = chain.shape[1]
     acors = np.empty((max_lag+1, ndim))
     if max_lag > len(chain)/5:
@@ -27,7 +33,7 @@ def exponential_function(lag, tao, offset):
     return np.exp(-lag/tao) + offset
 
 def autocorrelation_time(acors):
-    tao = scipy.optimize.curve_fit(exp_func, lags, acors)[0][0]
+    tao = scipy.optimize.curve_fit(exponential_function, lags, acors)[0][0]
     return tao
 
 def most_correlated(acors):
