5c04357d99d0c55200193701b0fb7f0beec8acce
Isak Svensson <isak.svensson@chalmers.se>
2019-10-12 07:28:54 +0200

    Fixed error in RWM.to_disk where the stepsize was accessed erroneously.

--- montepython/rwm.py
+++ montepython/rwm.py
@@ -16,8 +16,12 @@ class RWM(MCMC):
         # CREATE THE COVARIANCE MATRIX
         self._covariance = self._stepsize * np.eye(self.ndim())
 
+    @property
+    def stepsize(self):
+        return self._stepsize
+
     def to_disk(self, *args, **kwargs):
-        kwargs['stepsize'] = self._stepsize()
+        kwargs['stepsize'] = self.stepsize
         super().to_disk(*args, **kwargs)
 
     # STATE PROPOSAL
