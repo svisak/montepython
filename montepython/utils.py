3b03a95747f414365fde9828bd3ee90ee90bbc9b
Isak Svensson <isak.svensson@chalmers.se>
2020-07-09 22:46:48 +0200

    New method: run_for(t, unit)
    The method samples for the specified amount of time t, with the unit specified by 'unit'.
    Valid units are 'minutes', 'hours', and 'days'.

--- montepython/utils.py
+++ montepython/utils.py
@@ -7,6 +7,16 @@ import os
 def timestamp():
     return time.strftime("%Y%m%d_%H%M%S", time.gmtime())
 
+def convert_to_seconds(t, unit):
+    if unit == 'minutes':
+        return t * 60.0
+    elif unit == 'hours':
+        return t * 3600.0
+    elif unit == 'days':
+        return t * 86400.0
+    else:
+        raise ValueError('Unregognized time unit')
+
 def mcmc_to_disk(mcmc, **kwargs):
     """Save the MCMC chain to disk with the metadata supplied in kwargs."""
 
